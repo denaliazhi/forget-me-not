0669f8ad70a76b0d41ce794cf5434d5f4405d127
Denalia Zhi <d.naliazhi@gmail.com>
2025-09-05 19:19:57 -0400

    style: prettify directions modal and end game message

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,30 @@ function App() {
 
   return (
     <>
-      <h1>Forget Me Not</h1>
-      <Directions
-        show={showDialog}
-        setShow={setShowDialog}
-        play={isPlaying}
-        setPlay={setIsPlaying}
-      >
-        Welcome to our garden 🌷. The game is simple: 1. Pick (click on) a
-        flower to grow your score. 2. Pick each flower once, no more. 3. Picking
-        twice is not very nice! We'll send you out the door.
-      </Directions>
-      <Game play={isPlaying} setPlay={setIsPlaying}></Game>
-      <div className="game-bar">
+      <div>
+        <span id="title"></span>
+        <Directions
+          show={showDialog}
+          setShow={setShowDialog}
+          play={isPlaying}
+          setPlay={setIsPlaying}
+        >
+          {`Welcome to our garden 🌷. The game is simple:
+          1. Pick (click on) a flower to grow your score. 
+          2. Pick each flower once, no more.
+          3. Picking twice is not very nice! We'll send you to the door.`}
+        </Directions>
+        <Game play={isPlaying} setPlay={setIsPlaying}></Game>
         <button className="directions-btn" onClick={() => setShowDialog(true)}>
-          How To Play
+          ?
         </button>
       </div>
+      <footer>
+        <p>
+          Powered by <a href="https://developers.giphy.com/">GIPHY</a>
+          <span></span>
+        </p>
+      </footer>
     </>
   );
 }
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,11 +1,13 @@
 export function Message({ title, content, sticker, handleClick }) {
   return (
     <div className="msg-bar">
-      <h3>{title}</h3>
+      {sticker && <img src={sticker} />}
       <div>
+        <h3>{title}</h3>
+
         <p>{content}</p>
-        {sticker && <img src={sticker} />}
       </div>
+
       <button onClick={handleClick}>Play again</button>
     </div>
   );
